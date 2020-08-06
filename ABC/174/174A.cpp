1a9d01cba8f09e066dfbd9990205be5b39fecbe8
jimar884 <60651943+jimar884@users.noreply.github.com>
2020-08-06 10:39:17 +0900

    Create 174A.cpp

---
+++ ABC/174/174A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int X;
+
+    cin >> X;
+
+    if(X>=30){
+        cout << "Yes" << endl;
+    } else{
+        cout << "No" << endl;
+    }
+}
